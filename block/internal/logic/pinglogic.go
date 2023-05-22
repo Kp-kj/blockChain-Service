f8ae60f439e875c2cd136477e3b051328dd9fef0
Ed <40153785+China-Chris@users.noreply.github.com>
2023-05-22 13:32:53 +0800

    feat: initialization blockChain_server

---
+++ block/internal/logic/pinglogic.go
@@ -0,0 +1,32 @@
+package logic
+
+import (
+	"context"
+
+	"block/block"
+	"block/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PingLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
+	return &PingLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *PingLogic) Ping(in *block.Request) (*block.Response, error) {
+	// todo: add your logic here and delete this line
+	// 测试连接
+	return &block.Response{
+		Pong: "pong",
+	}, nil
+}
