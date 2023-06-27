aa41ea2d36f1f6ed84682477c8cca4dff13faa39
王世佳 <wangshijia612@163.com>
2023-06-27 10:19:00 +0800

    feat: add block.proto
    生成了部分rpc接口（未完成接口内部），删除了部分api生成的相关文件

---
+++ block/internal/logic/getgoodslistlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"block/block/block"
+	"block/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetGoodsListLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetGoodsListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetGoodsListLogic {
+	return &GetGoodsListLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetGoodsListLogic) GetGoodsList(in *block.GetGoodsListRequest) (*block.GetGoodsListResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &block.GetGoodsListResponse{}, nil
+}
